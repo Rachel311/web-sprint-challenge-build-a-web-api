32083e0246f70a7eb38ab834424fbcfe95e41b3c
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-09-10 20:59:02 -0600

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,33 @@
 // Write your "projects" router here!
+const express = require('express');
+const router = require('express').Router();
+const Project = require('./projects-model');
+
+const {
+    validateProjectId,
+    validateProject,
+} = require('../projects/projects-middleware');
+
+
+// Returns and array of all projects
+router.get('/', (req, res, next) => {
+   Project.get()
+    .then((projects) => {
+        res.json(projects);
+    })
+    .catch(next);
+});
+
+router.get('/:id', validateProjectId, (req, res) => {
+    res.json(req.projects);
+});
+
+router.post('/', validateProject, (req, res) => {
+    Project.insert({ name: req.name, description: req.description })
+        .then((newProject) => {
+            res.status(201).json(newProject);
+        })
+        .catch(next);
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const server = express();
 
+const helmet = require('helmet');
+const { logger } = require('./projects/projects-middleware');
+const projectsRouter = require('./projects/projects-router');
+
+server.use(express.json());
+server.use(helmet());
+server.use('/api/projects', projectsRouter);
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.get('/', (req, res) => {
+    res.send(`
+    <h1>My App is Working!</h1>
+    `)
+});
+
 
 module.exports = server;
